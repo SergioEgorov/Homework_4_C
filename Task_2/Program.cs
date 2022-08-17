8ba180b2baf861ba682154063091c28e7a42eaeb
Sergey <hojalata@mail.ru>
2022-08-17 13:58:59 +0300

    Выполнил задачу №27

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int GetSum (int number)
+{
+    int result = 0;
+    result += number % 10;
+    number /= 10;
+    if(number > 0)
+    {
+    result += GetSum(number);
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма чисел числа {number} = {GetSum(number)}");
