0c8830974718fd15830ab18c071b5792caaea1c3
Sergey <hojalata@mail.ru>
2022-08-17 15:52:53 +0300

    Выполнил задачу №29

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первый элемент массива: ");
+Console.WriteLine("Введите второй элемент массива: ");
+Console.WriteLine("Введите третий элемент массива: ");
+Console.WriteLine("Введите четвёртый элемент массива: ");
+Console.WriteLine("Введите пятый элемент массива: ");
+Console.WriteLine("Введите шестой элемент массива: ");
+Console.WriteLine("Введите седьмой элемент массива: ");
+Console.WriteLine("Введите восьмой элемент массива: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int number4 = Convert.ToInt32(Console.ReadLine());
+int number5 = Convert.ToInt32(Console.ReadLine());
+int number6 = Convert.ToInt32(Console.ReadLine());
+int number7 = Convert.ToInt32(Console.ReadLine());
+int number8 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Элементы массива [{number1} , {number2} , {number3} , {number4} , {number5} , {number6}, {number7} , {number8}]");
