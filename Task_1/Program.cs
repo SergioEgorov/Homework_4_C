dbc1b408dbabb849d4222cba850ff3dc87846f10
Sergey <hojalata@mail.ru>
2022-08-17 13:23:27 +0300

    Выполнил задачу №25

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число A: ");
+Console.WriteLine("Введите число B: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result =0;
+for (int i = 0; i <= numberB; i++)
+{
+    result ^=i;
+    Console.WriteLine($"Число {numberA} в степени {numberB} = {(long)Math.Pow(numberA,i)}");
+}
+
